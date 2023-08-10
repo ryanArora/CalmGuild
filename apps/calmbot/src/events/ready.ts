9b2584d18ec66ddadcf5410a896a275b8daf9310
Clayton <miqhtiedev@gmail.com>
2023-08-10 13:56:14 -0400

    automatically create guild entries to database

--- apps/calmbot/src/events/ready.ts
+++ apps/calmbot/src/events/ready.ts
@@ -1,8 +1,23 @@
 import { Event } from "../client/events";
+import { Prisma, client as database } from "database";
 
 const readyEvent: Event = {
-  execute: (client) => {
+  execute: async (client) => {
     console.log(`${client.user?.tag} is online`);
+
+    // Create guild entry in database if it doesn't exist
+    const guilds = client.guilds.cache;
+    const guildData = await database.guild.findMany();
+
+    const toAdd: Prisma.GuildCreateManyInput[] = [];
+
+    for (const [, guild] of guilds) {
+      if (guildData.find((g) => g.guildId === guild.id)) continue;
+
+      toAdd.push({ guildId: guild.id });
+    }
+
+    if (toAdd.length !== 0) await database.guild.createMany({ data: toAdd }).then((batch) => console.log(`Created ${batch.count} new guild documents`));
   },
   type: "ready",
 };
--- libs/database/src/index.ts
+++ libs/database/src/index.ts
@@ -1,6 +1,7 @@
 import type { User as PrismaUser, RoleType as PrismaRoleType, ChannelType as PrismaChannelType, SkullboardMessage as PrismaSkullboardMessageType } from "@prisma/client";
 
 export * from "./lib/database";
+export * from "@prisma/client";
 
 export type User = PrismaUser;
 export type RoleType = PrismaRoleType;
