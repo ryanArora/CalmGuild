7438c5d3b7becdb6644c31723239e5b9af1d9cbf
Clayton <miqhtiedev@gmail.com>
2023-08-11 17:33:14 -0400

    create differenciation between guild based and user based data (Member vs User)

--- libs/database/src/index.ts
+++ libs/database/src/index.ts
@@ -1,9 +1,10 @@
-import type { User as PrismaUser, RoleType as PrismaRoleType, ChannelType as PrismaChannelType, SkullboardMessage as PrismaSkullboardMessageType } from "@prisma/client";
+import type { User as PrismaUser, Member as PrismaMember, RoleType as PrismaRoleType, ChannelType as PrismaChannelType, SkullboardMessage as PrismaSkullboardMessageType } from "@prisma/client";
 
 export * from "./lib/database";
 export * from "@prisma/client";
 
 export type User = PrismaUser;
+export type Member = PrismaMember;
 export type RoleType = PrismaRoleType;
 export type ChannelType = PrismaChannelType;
 export type SkullboardMessage = PrismaSkullboardMessageType;
