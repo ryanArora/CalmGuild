7d407083ee44cbe895982ae8a5243cf71a58477f
Clayton <miqhtiedev@gmail.com>
2023-08-29 23:10:39 -0400

    improve efficiency sendDmFromChannel (again!)

--- apps/calmbot/src/utils/sendDmOrChannel.ts
+++ apps/calmbot/src/utils/sendDmOrChannel.ts
@@ -4,11 +4,13 @@ import getChannel from "./getChannel";
 
 // Attempts to DM a user a message,
 // if this user has their DMs closed it will send the message in the fallback channel provided
-export default async (client: Client, userId: string, guild: Guild, channel: ChannelType | TextChannel, message: MessageCreateOptions) => {
+export default async (client: Client, userId: string, guild: Guild, channel?: ChannelType | TextChannel, message: MessageCreateOptions) => {
   return new Promise(async () => {
     const user = client.users.cache.get(userId) ?? (await client.users.fetch(userId));
 
     user.send(message).catch(async () => {
+      if (!channel) return;
+
       if (channel instanceof TextChannel) {
         channel.send(message);
         return;
