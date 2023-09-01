d8d6185fb89d568021be6b462ac464edb02fc881
Clayton <miqhtiedev@gmail.com>
2023-08-31 23:10:53 -0400

    update typings

--- apps/calmbot/typings/env.d.ts
+++ apps/calmbot/typings/env.d.ts
@@ -3,5 +3,7 @@ namespace NodeJS {
   interface ProcessEnv {
     readonly BOT_TOKEN: string;
     readonly HYPIXEL_API_KEY: string;
+    readonly MINECRAFT_BOT_SERVER_KEY: string;
+    readonly MINECRAFT_BOT_SERVER_URL: string;
   }
 }
