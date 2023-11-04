9f10788c49f4960edb9dc1b76a5c6c8c0871389b
Clayton <miqhtiedev@gmail.com>
2023-11-03 20:44:19 -0400

    announce loyalty promotions weekly

--- apps/calmbot/src/tasks/giveLoyaltyRoles.ts
+++ apps/calmbot/src/tasks/giveLoyaltyRoles.ts
@@ -1,10 +1,8 @@
-// Give roles to members for being in the guild for 6 months & 12 months
-
 import { Collection } from "discord.js";
 import { getGuild } from "../utils/apis/hypixel";
 import { Task } from "../utils/startTasks";
 import { client as database } from "database";
-import getChannel from "../utils/getChannel";
+
 const task: Task = {
   execute: async (client) => {
     const guild = client.guilds.cache.get(process.env.DEFAULT_DISCORD_SERVER_ID);
@@ -27,7 +25,6 @@ const task: Task = {
     const userData = await database.user.findMany({ where: { minecraftUuid: { in: Array.from(loyaltyRoleMap.values()).flat() } }, select: { minecraftUuid: true, discordId: true } });
     await guild.members.fetch({ user: userData.map((u) => u.discordId) });
 
-    let message = "";
     for (const [role, members] of loyaltyRoleMap) {
       if (members.length === 0) continue;
 
@@ -37,15 +34,10 @@ const task: Task = {
       const membersRecievingRole = guild.members.cache.filter((member) => userData.filter((u) => members.includes(u.minecraftUuid ?? "")).find((u) => u.discordId === member.id) && !member.roles.cache.get(role));
       if (membersRecievingRole.size === 0) continue;
 
-      message += `## ${discordRole.name}:\n`;
       for (const [, member] of membersRecievingRole) {
         member.roles.add(discordRole);
-        message += `- ${member.user} \n`;
       }
     }
-
-    const channel = await getChannel("GUILD_ANNOUNCEMENTS", guild);
-    if (channel?.isTextBased() && message !== "") channel.send(`Congratulations to the following member(s) on their promotions:\n${message}`);
   },
   cronExpression: "0 12 * * *",
 };
